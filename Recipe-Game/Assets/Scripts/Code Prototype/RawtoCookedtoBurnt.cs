a4e1edf6d54985e8e65f3902b645969e3d179b24
Thomas Robinson <Thomasr.g64@gmail.com>
2024-10-19 23:40:59 -0600

    started working on the raw->ccooked->burnt function. Added a script for colour changing, but need to start adding the property management for the ingredients to finish. Dread. Dread. Courage.

---
+++ Recipe-Game/Assets/Scripts/Code Prototype/RawtoCookedtoBurnt.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RawtoCookedtoBurnt : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
